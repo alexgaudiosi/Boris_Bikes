5ce9787cc2ce48fdc1db0948b1407e70ce1f207e
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-03-03 17:00:11 +0000

    Expanded on 'Bike' class

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,3 +1,22 @@
 class Bike
 
+	# the intitialize method is always called when you create a new
+	# class by typing Bike.new
+	def initialize
+		# all instance variables beging with "@"
+		# this must be an instance variable because we'll need it
+		# in other methods
+		@broken = false
+	end
+
+	def broken?
+		# instance variables are accessible in all methods
+		@broken
+	end
+
+	def break
+		# and any instance method can update them
+		@broken = true
+	end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -11,4 +11,10 @@ describe Bike do |variable|
 		# a method "broken?" that should return false
 		expect(bike).not_to be_broken
 	end
+
+	it "should be able to break" do
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
+	end
 end
