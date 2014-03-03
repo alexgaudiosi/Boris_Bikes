90919968a3a61024e962436fe3d38e426246ab41
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-03-03 16:33:59 +0000

    First commit

---
+++ spec/bike_spec.rb
@@ -0,0 +1,14 @@
+# link to the Bike Class
+require_relative "../lib/bike"
+
+# we're describing the functionality of a specific class, Bike
+describe Bike do |variable|
+	#this is a specific feature (behaviour)
+	# that we expect to be present
+	it "should not be broken after we create it" do
+		bike = Bike.new # intialise a new object of Bike Class
+		# expect an instance of this class to have
+		# a method "broken?" that should return false
+		expect(bike).not_to be_broken
+	end
+end
