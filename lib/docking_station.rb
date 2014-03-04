37b47e495e405871ac885b460b80d14d909e204c
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-03-04 07:56:57 +0000

    Docking station capacities added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,5 @@
 class DockingStation
+	DEFAULT_CAPACITY = 10
 
 	def initialize
 		@bikes = []
@@ -14,6 +15,15 @@ class DockingStation
 
 	def release(bike)
 		@bikes.delete(bike)
+	end
+
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+		@bikes = []
+	end
+
+	def full?
+		bike_count == @capacity
 	end
 
 end
