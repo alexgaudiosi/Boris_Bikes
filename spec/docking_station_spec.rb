6f113c48547718689743a9f339ea77ac51ff6494
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-03-04 08:32:25 +0000

    Error raised if station full, some test refactoring

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -24,10 +24,19 @@ describe DockingStation do
 		expect(station.bike_count).to eq(0)
 	end
 
+	def fill_station(station)
+		20.times { station.dock(Bike.new)}
+	end
+
 	it "should know when it's full" do
 		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
+		fill_station station
 		expect(station).to be_full
+	end
+
+	it "should not accept a bike if it's full" do
+		fill_station station
+		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
 	end
 
 end
